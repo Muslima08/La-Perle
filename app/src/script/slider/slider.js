31ef08dde2e49359cd9d6b7cfc20d6137b3a5f38
muslima <azimovamuslima145@gmail.com>
2022-03-25 11:50:00 +0500

    menu

---
+++ app/src/script/slider/slider.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     // инициализация слайдера
+//     var slider = new SimpleAdaptiveSlider('.slider', {
+//       loop: false,
+//       autoplay: false,
+//       interval: 5000,
+//       swipe: true,
+//     });
+//   });
